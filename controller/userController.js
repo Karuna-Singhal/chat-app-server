1715f4ce98d588431c56a629d51b5f962f6edbb7
Karuna <singhalkaruna73@gmail.com>
2023-07-16 00:20:52 +0530

    login Controller

--- controller/userController.js
+++ controller/userController.js
@@ -23,3 +23,20 @@ module.exports.register = async (req, res, next) => {
     next(err);
   }
 };
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordValid = await bcrypt.compare(password, user?.password);
+    if (!user) {
+      return res.json({ msg: "Incorrect username or Password", status: false });
+    }
+    if (!isPasswordValid) {
+      return res.json({ msg: "Incorrect username or Password", status: false });
+    }
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (err) {
+    next(err);
+  }
+};
