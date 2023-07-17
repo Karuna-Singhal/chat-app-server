dd344d6fa5edbe1874d45e06a338463c1ad84dc9
Karuna <singhalkaruna73@gmail.com>
2023-07-17 12:14:49 +0530

    setup socket

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const msgRoutes = require("./routes/msgRoutes");
+const socket = require("socket.io");
 
 const app = express();
 require("dotenv").config();
@@ -27,4 +28,24 @@ mongoose
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server Started on Port ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://127.0.0.1:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.io);
+  });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.io);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
 });
